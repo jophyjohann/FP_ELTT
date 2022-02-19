418c7ceb2fd0f53e5c643df977eced23181185b5
jophyjohann <94175623+jophyjohann@users.noreply.github.com>
2022-02-19 21:21:37 +0000

    x

--- script4.py
+++ script4.py
@@ -1,6 +1,7 @@
 from dataset_operations2 import DataSet_Operations2
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 import os
 
 class run:
@@ -21,19 +22,21 @@ class run:
 			mng.resize(*mng.window.maxsize())
 			#mng.full_screen_toggle()
 			#os.system('xdotool key alt+F10')
-
-		### Plot all measured datasets (14) and export them for overwiev ###
+		
+		def logistic(x, a, b, c, d):
+			return a / np.sqrt(1 + np.exp(b * (x + c))) + d
+		
+		### Plot
 
 		data = dataSet[1]
-
-		plot_lims = [[700,1000],
-								[1150,1450],
-								[1500,1750],
-								[2200,2450],
-								[2800,3050],
-								[3200,3500],
-								[3700,4050]]
 			
+		plot_lims = [[700,950],
+								[1050,1450],
+								[1450,1850],
+								[1850,2700],
+								[2650,3150],
+								[3150,3600],
+								[3550,4350]]
 		
 		# set plot characteristics of x and y ticks and grid
 		major_x_ticks = 500
@@ -42,37 +45,72 @@ class run:
 		minor_y_ticks = 0.001
 		major_alpha = 0.7
 		minor_alpha = 0.2
-
+			
+		T_data = data['T'][plot_lims[0][0]:plot_lims[0][1]]
+		t_data = data['t'][plot_lims[0][0]:plot_lims[0][1]]
+		R_data = data['R_P_1'][plot_lims[0][0]:plot_lims[0][1]]
+		
 		fig = plt.figure(figsize=(8, 4), dpi=120).add_subplot(1, 1, 1)
-		plt.plot(data['t'], data['R_P_1'], '-')
+		plt.plot(t_data, R_data, '-')
+		#plt.plot(t_data, R_data, '-')
 		plt.xlabel("T / K")
 		plt.ylabel(r"R / $\Omega$")
-		plt.xlim(0,None)
+		#plt.xlim(0,None)
 		plt.ylim(0,None)
 		fig.set_xticks(np.arange(plt.xlim()[0],plt.xlim()[1],major_x_ticks))
 		fig.set_xticks(np.arange(plt.xlim()[0],plt.xlim()[1],minor_x_ticks),minor=True)
-		fig.set_yticks(np.arange(plt.ylim()[0],plt.ylim()[1],major_y_ticks))
-		fig.set_yticks(np.arange(plt.ylim()[0],plt.ylim()[1],minor_y_ticks),minor=True)
+		#fig.set_yticks(np.arange(plt.ylim()[0],plt.ylim()[1],major_y_ticks))
+		#fig.set_yticks(np.arange(plt.ylim()[0],plt.ylim()[1],minor_y_ticks),minor=True)
 		fig.grid(which='minor', alpha=minor_alpha)
 		fig.grid(which='major', alpha=major_alpha)
 		maximize()
 		plt.show()
 
-			
+
+		
 		print(80*"_"+"\n\nPlotting: Tc Plot für I = 0A")
+
+		# fitting the function
+		plot_range = [None, None]
+		fit_range = [None, None]
+
+		T_data = data['T'][plot_lims[0][0]:plot_lims[0][1]]
+		R_data = data['R_P_1'][plot_lims[0][0]:plot_lims[0][1]]
+
+		T_data = T_data[plot_range[0]:plot_range[1]]
+		R_data = R_data[plot_range[0]:plot_range[1]]
+
+		data = [T_data, R_data]
+		
+		#data = np.sort(data)
+		T_data = data[0]
+		R_data = data[1]
+		
 			
+		fit_parameters = [["a","b",  "c","d"],
+										  [ 0,  20, -6.9, 0.0655],		 # max bounds
+										  [-0.01,  10, -9, 0.015],		 # start values
+										  [-0.09, 0.1, -8.9,  0.06]]		 # min bounds
+		
+		popt, pcov = curve_fit(logistic, T_data[fit_range[0]:fit_range[1]], R_data[fit_range[0]:fit_range[1]], fit_parameters[2])#, bounds=(fit_parameters[3],fit_parameters[1]))  
+		
+		print("\nLogistic Fkt. y = a/(1+exp(b*x +c) +d ")
+		print("a = {:.4g} +\- {:.4g}\nb= {:.4g} +\- {:.4g}\nc= {:.4g} +\- {:.4g}\nd= {:.4g} +\- {:.4g}".format(popt[0], np.sqrt(np.diag(pcov))[0], popt[1], np.sqrt(np.diag(pcov))[1], popt[2], np.sqrt(np.diag(pcov))[2], popt[3], np.sqrt(np.diag(pcov))[3]))
+		
+		
 		fig = plt.figure(figsize=(8, 4), dpi=120).add_subplot(1, 1, 1)
-		plt.plot(data['t'], data['R_P_1'], '-')
+		plt.plot(T_data, R_data, '.')
+		plt.plot(T_data[fit_range[0]:fit_range[1]], logistic(T_data[fit_range[0]:fit_range[1]], *popt), '--', label = r"Logistic func. fit")
 		plt.xlabel("T / K")
 		plt.ylabel(r"R / $\Omega$")
-		plt.xlim(plot_lims[0][0],plot_lims[0][1])
+		#plt.xlim(0,None)
 		#plt.ylim(0,None)
-		fig.set_xticks(np.arange(plt.xlim()[0],plt.xlim()[1],major_x_ticks))
-		fig.set_xticks(np.arange(plt.xlim()[0],plt.xlim()[1],minor_x_ticks),minor=True)
-		fig.set_yticks(np.arange(plt.ylim()[0],plt.ylim()[1],major_y_ticks))
-		fig.set_yticks(np.arange(plt.ylim()[0],plt.ylim()[1],minor_y_ticks),minor=True)
-		fig.grid(which='minor', alpha=minor_alpha)
-		fig.grid(which='major', alpha=major_alpha)
+		#fig.set_xticks(np.arange(plt.xlim()[0],plt.xlim()[1],major_x_ticks))
+		#fig.set_xticks(np.arange(plt.xlim()[0],plt.xlim()[1],minor_x_ticks),minor=True)
+		#fig.set_yticks(np.arange(plt.ylim()[0],plt.ylim()[1],major_y_ticks))
+		#fig.set_yticks(np.arange(plt.ylim()[0],plt.ylim()[1],minor_y_ticks),minor=True)
+		#fig.grid(which='minor', alpha=minor_alpha)
+		#fig.grid(which='major', alpha=major_alpha)
 		maximize()
 		plt.show()
 
