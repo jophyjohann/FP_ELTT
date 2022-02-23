3cba2f8259ea91a9e9ddc5937804e78dbee8c6f7
jophyjohann <94175623+jophyjohann@users.noreply.github.com>
2022-02-23 23:08:16 +0000

    x

--- script5.py
+++ script5.py
@@ -67,11 +67,13 @@ class run:
 		search_points = R_data[fit_range_os_l[1]:fit_range_os_l[0]]
 		R_10p = search_points[np.abs(search_points-R_p_10p).argmin()]
 		Tc_10p = min(T_data[np.where(R_data==R_10p)])
-			
+		print("Tc @ 10% = ",Tc_10p)
+		
 		R_p_90p = (popt_os_r[0] - popt_os_l[0]) * 0.9 + popt_os_l[0]
 		search_points = R_data[fit_range_os_r[1]:fit_range_os_r[0]]
 		R_90p = search_points[np.abs(search_points-R_p_90p).argmin()]
 		Tc_90p = min(T_data[np.where(R_data==R_90p)])
+		print("Tc @ 90% = ",Tc_90p)
 		
 		fig = plt.figure(figsize=(8, 4), dpi=160).add_subplot(1, 1, 1)
 		plt.plot(T_data, R_data, '.')
@@ -91,16 +93,16 @@ class run:
 		print(80*"_"+"\n\nPlotting: Tc Plot für I = 1A")
 
 		# fitting the function
-		plot_range = [0, 800]
+		plot_range = [240, 600]
 		fit_range = [None, None]
 		
-		fit_range_os_l = [None, None]
+		fit_range_os_l = [None, 150]
 		fit_plot_range_os_l = [None, None]
-		fit_plot_range_os_l = fit_range_os_l
+		#fit_plot_range_os_l = fit_range_os_l
 		
-		fit_range_os_r = [None, None]
+		fit_range_os_r = [250, None]
 		fit_plot_range_os_r = [None, None]
-		fit_plot_range_os_r = fit_range_os_r
+		#fit_plot_range_os_r = fit_range_os_r
 			
 		data = dataSet[2]
 		t_data = data['t']
@@ -118,15 +120,176 @@ class run:
 		search_points = R_data[fit_range_os_l[1]:fit_range_os_l[0]]
 		R_10p = search_points[np.abs(search_points-R_p_10p).argmin()]
 		Tc_10p = min(T_data[np.where(R_data==R_10p)])
+		print("Tc @ 10% = ",Tc_10p)
 			
 		R_p_90p = (popt_os_r[0] - popt_os_l[0]) * 0.9 + popt_os_l[0]
 		search_points = R_data[fit_range_os_r[1]:fit_range_os_r[0]]
 		R_90p = search_points[np.abs(search_points-R_p_90p).argmin()]
 		Tc_90p = min(T_data[np.where(R_data==R_90p)])
+		print("Tc @ 90% = ",Tc_90p)
 		
 		fig = plt.figure(figsize=(8, 4), dpi=160).add_subplot(1, 1, 1)
 		plt.plot(T_data, R_data, '.')
-		#T_data = np.linspace(T_data[0],T_data[-1],1000)
+		T_data = np.linspace(T_data[0],T_data[-1],1000)
+		plt.plot(T_data[fit_plot_range_os_l[0]:fit_plot_range_os_l[1]], offset(T_data[fit_plot_range_os_l[0]:fit_plot_range_os_l[1]], *popt_os_l), '--')
+		plt.plot(T_data[fit_plot_range_os_r[0]:fit_plot_range_os_r[1]], offset(T_data[fit_plot_range_os_r[0]:fit_plot_range_os_r[1]], *popt_os_r), '--')
+		plt.plot(Tc_10p, R_10p, 'o')
+		plt.plot(Tc_90p, R_90p, 'o')
+		plt.xlabel("T / K")
+		plt.ylabel(r"R / $\Omega$")
+		#plt.title("Tc Plot für I = 0A")
+		maximize()
+		plt.show()
+
+
+			
+		print(80*"_"+"\n\nPlotting: Tc Plot für I = 2A")
+
+		# fitting the function
+		plot_range = [670, 770]
+		fit_range = [None, None]
+		
+		fit_range_os_l = [-50, None]
+		fit_plot_range_os_l = [None, None]
+		#fit_plot_range_os_l = fit_range_os_l
+		
+		fit_range_os_r = [None, -65]
+		fit_plot_range_os_r = [None, None]
+		#fit_plot_range_os_r = fit_range_os_r
+			
+		data = dataSet[2]
+		t_data = data['t']
+		T_data = data['T']
+		R_data = data['R_P_1']
+
+		t_data = t_data[plot_range[0]:plot_range[1]]
+		T_data = T_data[plot_range[0]:plot_range[1]]
+		R_data = R_data[plot_range[0]:plot_range[1]]
+
+		popt_os_l, pcov_os_l = curve_fit(offset, T_data[fit_range_os_l[0]:fit_range_os_l[1]], R_data[fit_range_os_l[0]:fit_range_os_l[1]])
+		popt_os_r, pcov_os_r = curve_fit(offset, T_data[fit_range_os_r[0]:fit_range_os_r[1]], R_data[fit_range_os_r[0]:fit_range_os_r[1]])
+
+		R_p_10p = (popt_os_r[0] - popt_os_l[0]) * 0.1 + popt_os_l[0]
+		search_points = R_data[fit_range_os_l[1]:fit_range_os_l[0]]
+		R_10p = search_points[np.abs(search_points-R_p_10p).argmin()]
+		Tc_10p = min(T_data[np.where(R_data==R_10p)])
+		print("Tc @ 10% = ",Tc_10p)
+			
+		R_p_90p = (popt_os_r[0] - popt_os_l[0]) * 0.9 + popt_os_l[0]
+		search_points = R_data[fit_range_os_r[1]:fit_range_os_r[0]]
+		R_90p = search_points[np.abs(search_points-R_p_90p).argmin()]
+		Tc_90p = min(T_data[np.where(R_data==R_90p)])
+		print("Tc @ 90% = ",Tc_90p)
+		
+		fig = plt.figure(figsize=(8, 4), dpi=160).add_subplot(1, 1, 1)
+		plt.plot(T_data, R_data, '.')
+		T_data = np.linspace(T_data[0],T_data[-1],1000)
+		plt.plot(T_data[fit_plot_range_os_l[0]:fit_plot_range_os_l[1]], offset(T_data[fit_plot_range_os_l[0]:fit_plot_range_os_l[1]], *popt_os_l), '--')
+		plt.plot(T_data[fit_plot_range_os_r[0]:fit_plot_range_os_r[1]], offset(T_data[fit_plot_range_os_r[0]:fit_plot_range_os_r[1]], *popt_os_r), '--')
+		plt.plot(Tc_10p, R_10p, 'o')
+		plt.plot(Tc_90p, R_90p, 'o')
+		plt.xlabel("T / K")
+		plt.ylabel(r"R / $\Omega$")
+		#plt.title("Tc Plot für I = 0A")
+		maximize()
+		plt.show()
+
+
+			
+		print(80*"_"+"\n\nPlotting: Tc Plot für I = 3A")
+
+		# fitting the function
+		plot_range = [970, 1100]
+		fit_range = [None, None]
+		
+		fit_range_os_l = [None, 75]
+		fit_plot_range_os_l = [None, None]
+		#fit_plot_range_os_l = fit_range_os_l
+		
+		fit_range_os_r = [95, None]
+		fit_plot_range_os_r = [None, None]
+		#fit_plot_range_os_r = fit_range_os_r
+			
+		data = dataSet[2]
+		t_data = data['t']
+		T_data = data['T']
+		R_data = data['R_P_1']
+
+		t_data = t_data[plot_range[0]:plot_range[1]]
+		T_data = T_data[plot_range[0]:plot_range[1]]
+		R_data = R_data[plot_range[0]:plot_range[1]]
+
+		popt_os_l, pcov_os_l = curve_fit(offset, T_data[fit_range_os_l[0]:fit_range_os_l[1]], R_data[fit_range_os_l[0]:fit_range_os_l[1]])
+		popt_os_r, pcov_os_r = curve_fit(offset, T_data[fit_range_os_r[0]:fit_range_os_r[1]], R_data[fit_range_os_r[0]:fit_range_os_r[1]])
+
+		R_p_10p = (popt_os_r[0] - popt_os_l[0]) * 0.1 + popt_os_l[0]
+		search_points = R_data[fit_range_os_l[1]:fit_range_os_l[0]]
+		R_10p = search_points[np.abs(search_points-R_p_10p).argmin()]
+		Tc_10p = min(T_data[np.where(R_data==R_10p)])
+		print("Tc @ 10% = ",Tc_10p)
+			
+		R_p_90p = (popt_os_r[0] - popt_os_l[0]) * 0.9 + popt_os_l[0]
+		search_points = R_data[fit_range_os_r[1]:fit_range_os_r[0]]
+		R_90p = search_points[np.abs(search_points-R_p_90p).argmin()]
+		Tc_90p = min(T_data[np.where(R_data==R_90p)])
+		print("Tc @ 90% = ",Tc_90p)
+		
+		fig = plt.figure(figsize=(8, 4), dpi=160).add_subplot(1, 1, 1)
+		plt.plot(T_data, R_data, '.')
+		T_data = np.linspace(T_data[0],T_data[-1],1000)
+		plt.plot(T_data[fit_plot_range_os_l[0]:fit_plot_range_os_l[1]], offset(T_data[fit_plot_range_os_l[0]:fit_plot_range_os_l[1]], *popt_os_l), '--')
+		plt.plot(T_data[fit_plot_range_os_r[0]:fit_plot_range_os_r[1]], offset(T_data[fit_plot_range_os_r[0]:fit_plot_range_os_r[1]], *popt_os_r), '--')
+		plt.plot(Tc_10p, R_10p, 'o')
+		plt.plot(Tc_90p, R_90p, 'o')
+		plt.xlabel("T / K")
+		plt.ylabel(r"R / $\Omega$")
+		#plt.title("Tc Plot für I = 0A")
+		maximize()
+		plt.show()
+
+
+
+		print(80*"_"+"\n\nPlotting: Tc Plot für I = 4A")
+
+		# fitting the function
+		plot_range = [1200, None]
+		fit_range = [None, None]
+		
+		fit_range_os_l = [None, 75]
+		fit_plot_range_os_l = [None, None]
+		#fit_plot_range_os_l = fit_range_os_l
+		
+		fit_range_os_r = [95, None]
+		fit_plot_range_os_r = [None, None]
+		#fit_plot_range_os_r = fit_range_os_r
+			
+		data = dataSet[2]
+		t_data = data['t']
+		T_data = data['T']
+		R_data = data['R_P_1']
+
+		t_data = t_data[plot_range[0]:plot_range[1]]
+		T_data = T_data[plot_range[0]:plot_range[1]]
+		R_data = R_data[plot_range[0]:plot_range[1]]
+
+		popt_os_l, pcov_os_l = curve_fit(offset, T_data[fit_range_os_l[0]:fit_range_os_l[1]], R_data[fit_range_os_l[0]:fit_range_os_l[1]])
+		popt_os_r, pcov_os_r = curve_fit(offset, T_data[fit_range_os_r[0]:fit_range_os_r[1]], R_data[fit_range_os_r[0]:fit_range_os_r[1]])
+
+		R_p_10p = (popt_os_r[0] - popt_os_l[0]) * 0.1 + popt_os_l[0]
+		search_points = R_data[fit_range_os_l[1]:fit_range_os_l[0]]
+		R_10p = search_points[np.abs(search_points-R_p_10p).argmin()]
+		Tc_10p = min(T_data[np.where(R_data==R_10p)])
+		print("Tc @ 10% = ",Tc_10p)
+			
+		R_p_90p = (popt_os_r[0] - popt_os_l[0]) * 0.9 + popt_os_l[0]
+		search_points = R_data[fit_range_os_r[1]:fit_range_os_r[0]]
+		R_90p = search_points[np.abs(search_points-R_p_90p).argmin()]
+		Tc_90p = min(T_data[np.where(R_data==R_90p)])
+		print("Tc @ 90% = ",Tc_90p)
+		
+		fig = plt.figure(figsize=(8, 4), dpi=160).add_subplot(1, 1, 1)
+		plt.plot(T_data, R_data, '.')
+		T_data = np.linspace(T_data[0],T_data[-1],1000)
 		plt.plot(T_data[fit_plot_range_os_l[0]:fit_plot_range_os_l[1]], offset(T_data[fit_plot_range_os_l[0]:fit_plot_range_os_l[1]], *popt_os_l), '--')
 		plt.plot(T_data[fit_plot_range_os_r[0]:fit_plot_range_os_r[1]], offset(T_data[fit_plot_range_os_r[0]:fit_plot_range_os_r[1]], *popt_os_r), '--')
 		plt.plot(Tc_10p, R_10p, 'o')
